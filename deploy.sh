befd68b229a0707c93e088f06804ecf3e22caf61
Dorsaf Mouissaoui <126805354+dorsafmwissaoui@users.noreply.github.com>
2024-04-29 11:09:02 +0000

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# Variables
-GIT_REMOTE="000webhost"
-GIT_BRANCH="master" # or your preferred branch
 
-# Directory containing your Symfony application
+GIT_REMOTE="000webhost"
+GIT_BRANCH="master" 
+
 LOCAL_DIR="symfony6"
 
-# Change to your Symfony app directory
+
 cd $LOCAL_DIR
 
-# Initialize Git repository if not already done
+
 git init
 
-# Add all files to the repository
+
 git add .
 
-# Commit changes
 git commit -m "Initial commit"
 
-# Add 000webhost Git remote
 git remote add $GIT_REMOTE https://github.com/000webhost/$GIT_REMOTE.git
 
-# Push to 000webhost
+
 git push $GIT_REMOTE $GIT_BRANCH --force
